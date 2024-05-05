96474b19495cc3487dfc0a1ac63a7d5260e02d5a
kondurusai <kondurusai863@gmail.com>
2024-05-05 11:52:18 +0530

    revealing module pattern is added

--- modulePattern.js
+++ modulePattern.js
@@ -16,3 +16,32 @@ const UIctrl = (function () {
   };
 })();
 UIctrl.CallchangeText();
+
+//---------------------------REVEALING MODULE PATTERN--------------------
+
+const Reveal = (function () {
+  let data = [];
+  function add(text) {
+    data.push(text);
+    console.log("the data is added--------");
+  }
+  function get(id) {
+    return data.find((item) => {
+      return item.id === id;
+    });
+  }
+  return {
+    add: add,
+    get: get,
+    data: data,
+  };
+})();
+
+Reveal.add({ id: 1, name: "john" });
+Reveal.add({ id: 2, name: "sai" });
+Reveal.add({ id: 3, name: "venkat" });
+console.log(Reveal.get(1));
+console.log(Reveal.get(2));
+console.log(Reveal.get(3));
+
+console.log(Reveal.data);
